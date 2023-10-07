620568b8539057d258b3bba24af26eca2706d615
YoshiPiker <stanekbalex@gmail.com>
2023-10-07 11:42:55 -0400

    build out the buildingspecs

--- Map/Building.cs
+++ Map/Building.cs
@@ -1,21 +1,78 @@
 using Godot;
 using System;
-
+using System.Collections.Generic;
+[Tool]
 public class Building : Node2D
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
-
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+
+	}
+	public BuildingSpecs buildingSpecs{get;set;}
+	public void setSprite(string spritePath, string scalingSprite)
+	{
+		((Sprite)GetNode("Sprite")).Texture = (Texture)GD.Load(spritePath);
+		//Build the scaling of the sprite. 
+	}
+	
+}
+public class BuildingSpecs
+{
+	public BuildingSpecs(int _level,
+							string _type,
+							string _placementType,
+							int _cargoSpace,
+							string _cargoHex,
+							int _maxLimit,
+							string _upgradeOf = "")
+	{
+		level = _level;
+		type = _type;
+		placementType = _placementType;
+		cargoSpace = _cargoSpace;
+		cargoHex = _cargoHex;
+		maxLimit = _maxLimit;
+		upgradeOf = _upgradeOf;
+	}
+	public int level { get; set; }
+	public string type { get; set; }
+	public string placementType { get; set; }
+	public int cargoSpace { get; set; }
+	public string cargoHex { get; set; }
+	public int maxLimit { get; set; }
+	public string upgradeOf { get; set; }
+	public List<BuildingRequirements> buildingRequirements { get; set; }
+	public List<BuildingProsume> buildingProsume { get; set; }
+}
+public class BuildingRequirements
+{
+	public BuildingRequirements(string _type,
+							string _tileType,
+							int _distance,
+							bool _negate)
+	{
+		type = _type;
+		tileType = _tileType;
+		distance = _distance;
+		negate = _negate;
+	}
+	public string type { get; set; }
+	public string tileType { get; set; }
+	public int distance { get; set; }
+	public bool negate { get; set; }
+}
+public class BuildingProsume
+{
+	public BuildingProsume(string _type,
+							string _function,
+							int _amount)
+	{
+		type = _type;
+		function = _function;
+		amount = _amount;
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	public string type { get; set; }
+	public string function { get; set; }
+	public int amount { get; set; }
 }
