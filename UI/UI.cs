eb3265b831ce554c605bf4e0496d35ac4877a8d7
cubone8055 <cubone8055@gmail.com>
2023-10-08 10:35:07 -0400

    plz work
    plz

--- UI/UI.cs
+++ UI/UI.cs
@@ -4,46 +4,50 @@ using System.Collections.Generic;
 
 public class UI : Control
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        gameState.Connect(nameof(GameState.OnPhaseChanged), this, nameof(OnPhaseChange));
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		gameState.Connect(nameof(GameState.OnPhaseChanged), this, nameof(OnPhaseChange));
+	}
 
-    private GameState gameState => GetNode<GameState>("/root/GameState");
+	private GameState gameState => GetNode<GameState>("/root/GameState");
 
-    private Button endTurnButton => GetNode<Button>("EndTurnButton");
+	private Button endTurnButton => GetNode<Button>("EndTurnButton");
 
-    [Signal]
-    public delegate void OnEndTurn();
+	[Signal]
+	public delegate void OnEndTurn();
 
-    [Signal]
-    public delegate void OnBuild(string buildingId);
+	[Signal]
+	public delegate void OnBuild(string buildingId);
 
-    public void OnStartBuilding(List<string> available) {
-        // TODO Alex & Nick
-    }
+	public void OnStartBuilding(List<string> available) {
+		// TODO Alex & Nick
+	}
 
-    private void _on_EndTurnButton_pressed() {
-        EmitSignal(nameof(OnEndTurn));
-    }
+	private void _on_EndTurnButton_pressed() {
+		EmitSignal(nameof(OnEndTurn));
+	}
 
-    public void OnPhaseChange() {
-        // TODO open/close various UI elements appropriately
-        endTurnButton.Visible = gameState.Phase == Phase.InTurn;
-    }
+	public void OnPhaseChange() {
+		// TODO open/close various UI elements appropriately
+		endTurnButton.Visible = gameState.Phase == Phase.InTurn;
 
-    public void _on_AboutButton_pressed(){
-        GetNode<Control>("InfoBox").Visible = true;
-    }
+		if (gameState.Phase == Phase.CargoDrop) {
+			// Do the things
+		}
+	}
 
-    public void _on_InfoCloseButton_pressed(){
-        GetNode<Control>("InfoBox").Visible = false;
-    }
+	public void _on_AboutButton_pressed(){
+		GetNode<Control>("InfoBox").Visible = true;
+	}
+
+	public void _on_InfoCloseButton_pressed(){
+		GetNode<Control>("InfoBox").Visible = false;
+	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
