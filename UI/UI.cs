27a98e628b2ab4153ba2a9d75935d9a69f4aaf3e
cubone8055 <cubone8055@gmail.com>
2023-10-08 11:16:25 -0400

    turn phases close menus
    and open them too!

--- UI/UI.cs
+++ UI/UI.cs
@@ -18,6 +18,12 @@ public class UI : Control
 
 	private Button endTurnButton => GetNode<Button>("EndTurnButton");
 
+	private Button CargoBuildingMenu => GetNode<Button>("/root/Map/CanvasLayer/Control/CargoUI/GridContainer/Button");
+	
+	private Button Inventory => GetNode<Button>("/root/Map/CanvasLayer/Control/InventoryUI/GridContainer/CloseMenuButton");
+	
+	private Button ResourceUI => GetNode<Button>("/root/Map/CanvasLayer/Control/ResourceUI/GridContainer/Button");
+
 	[Signal]
 	public delegate void OnEndTurn();
 
@@ -37,7 +43,68 @@ public class UI : Control
 		endTurnButton.Visible = gameState.Phase == Phase.InTurn;
 
 		if (gameState.Phase == Phase.CargoDrop) {
-			// Do the things
+			if(CargoBuildingMenu.Text != "X")
+				CargoBuildingMenu.Call("ToggleMenu");
+			if(Inventory.Text == "X")
+				Inventory.Call("ToggleMenu");
+			if(ResourceUI.Text == "X")
+				ResourceUI.Call("ToggleMenu");
+		}
+
+		if (gameState.Phase == Phase.DecidingOnBuildings) {
+			if(CargoBuildingMenu.Text == "X")
+				CargoBuildingMenu.Call("ToggleMenu");
+			if(Inventory.Text != "X")
+				Inventory.Call("ToggleMenu");
+			if(ResourceUI.Text == "X")
+				ResourceUI.Call("ToggleMenu");
+		}
+
+		if (gameState.Phase == Phase.Income) {
+			if(CargoBuildingMenu.Text == "X")
+				CargoBuildingMenu.Call("ToggleMenu");
+			if(Inventory.Text == "X")
+				Inventory.Call("ToggleMenu");
+			if(ResourceUI.Text != "X")
+				ResourceUI.Call("ToggleMenu");
+			System.Threading.Thread.Sleep(3000);
+		}
+
+		if (gameState.Phase == Phase.InTurn) {
+			if(CargoBuildingMenu.Text == "X")
+				CargoBuildingMenu.Call("ToggleMenu");
+			if(Inventory.Text == "X")
+				Inventory.Call("ToggleMenu");
+			if(ResourceUI.Text == "X")
+				ResourceUI.Call("ToggleMenu");
+		}
+
+		if (gameState.Phase == Phase.MovingRover) {
+			if(CargoBuildingMenu.Text == "X")
+				CargoBuildingMenu.Call("ToggleMenu");
+			if(Inventory.Text == "X")
+				Inventory.Call("ToggleMenu");
+			if(ResourceUI.Text == "X")
+				ResourceUI.Call("ToggleMenu");
+			System.Threading.Thread.Sleep(3000);
+		}
+
+		if (gameState.Phase == Phase.Scouting) {
+			if(CargoBuildingMenu.Text == "X")
+				CargoBuildingMenu.Call("ToggleMenu");
+			if(Inventory.Text == "X")
+				Inventory.Call("ToggleMenu");
+			if(ResourceUI.Text == "X")
+				ResourceUI.Call("ToggleMenu");
+		}
+
+		if (gameState.Phase == Phase.TargettingRover) {
+			if(CargoBuildingMenu.Text == "X")
+				CargoBuildingMenu.Call("ToggleMenu");
+			if(Inventory.Text == "X")
+				Inventory.Call("ToggleMenu");
+			if(ResourceUI.Text == "X")
+				ResourceUI.Call("ToggleMenu");
 		}
 	}
 
